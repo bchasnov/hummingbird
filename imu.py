8997f64c82851c5ecad23f554b9cd589d50ddceb
Alistair Dobke <adobke@gmail.com>
2015-07-11 16:16:54 -0700

    Add imu reading code

--- imu.py
+++ imu.py
@@ -2,34 +2,44 @@ import serial
 import threading
 
 class IMU(threading.Thread):
-    def __init__(self, port_name, baud_rate, debug=False):
-        super(RCIn, self).__init__()
+    def __init__(self, port_name, debug=False):
         # Debug Output Switch
         self.debug = debug
 
         # Port Setup
         self.port_name = port_name
-        self.baud_rate = baud_rate
-        self.port = serial.Serial(self.port_name, self.baud_rate, timeout=0)
+
+        self.port = serial.Serial(self.port_name, 115200, timeout=None)
+        self.port.write("$VNWRG,05,460800*XX\n")
+        self.port.close()
+        self.port = serial.Serial(self.port_name, 460800, timeout=None)
+        self.port.write("$VNWRG,06,8*XX\n")
+        self.port.write("$VNWRG,07,200*XX\n")
 
         # Status Flags
         self.connected = False
-        self.stop = False
+        self.stop = threading.Event()
 
         # RC Data
         self.num_inputs = 8
-        self.data = [0.0]*self.num_inputs
+        self.data = None
+
+        super(IMU, self).__init__()
 
     def run(self):
         """
         Override Thread.run(), called when self.start() is called
         :return: None
         """
-        while not self.stop:
+        while not self.stop.isSet():
             data = self.port.readline()
             if data:
                 self.handleData(data)
 
+    def halt(self):
+        self.stop.set()
+        self.port.close()
+        self.join()
 
     def handleData(self, data):
         """
@@ -39,8 +49,34 @@ class IMU(threading.Thread):
         """
         # Try getting self.num_inputs values as floats
         try:
-            data_float = map(float, data.split(','))
-            self.data = data_float[1:self.num_inputs+1]
+            if data[-1] != '\n' or data[0] != '$':
+                return
+            data = data.rstrip().split(",")
+            if data[0] != "$VNQMR":
+                if self.debug:
+                    print data
+                return
+            data = data[1:]
+            data[-1] = data[-1].split("*")[0]
+            data = map(float, data)
+            if len(data) != 13:
+              return
+            d = {
+                    "quat0": data[0],
+                    "quat1": data[1],
+                    "quat2": data[2],
+                    "quat3": data[3],
+                    "magx": data[4],
+                    "magy": data[5],
+                    "magx": data[6],
+                    "accelx": data[7],
+                    "accely": data[8],
+                    "accelx": data[9],
+                    "gyrox": data[10],
+                    "gyroy": data[11],
+                    "gyrox": data[12],
+                    }
+            self.data = d
         except IndexError:
             if self.debug:
                 print "RCIn: Couldn't read", self.num_inputs, "values"
